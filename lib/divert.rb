4d45882df77f5c867218d4560ace8d5af6a5c8a5
Erik <erik@hhd.com.au>
2017-10-13 10:29:46 +1100

    add divert_redirect_from parameter when redirecting from clientside

--- lib/divert.rb
+++ lib/divert.rb
@@ -15,6 +15,10 @@ module Divert
     yield(configuration)
   end
 
+  def hit_path(request)
+    request.fullpath.chomp("/")
+  end
+
   def action_missing path = nil
     controller = Divert.configuration.controller || controller_name
 
@@ -28,14 +32,17 @@ module Divert
     if template_exists?(path, [controller])
       render "#{controller}/#{path}" and return
     end
+   
+    divert = Redirect.find_or_create_by(hither: hit_path(request)) 
 
-    if Divert.configuration.save_to_db && (redirect = Redirect.hit(request.fullpath))
+    if Divert.configuration.save_to_db && (redirect = divert.hit)
       if Divert.configuration.redirect_clientside
 
         unless template_exists? 'divert_clientside.html.erb', [controller]
           render text:"Missing divert view: #{controller}/divert_clientside.html.erb", :status => 404 and return
         else
           params[:divert_redirect_to] = redirect
+          params[:divert_redirect_from] = divert.hither
           render "#{controller}/divert_clientside.html.erb", :layout => false, :status => 302 and return
         end
 
