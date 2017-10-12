3fed570d53d03d46703164511b07cede8df21bde
Erik <erik@hhd.com.au>
2017-10-12 11:43:34 +1100

    add_redirect_from_view to config

--- lib/divert/configuration.rb
+++ lib/divert/configuration.rb
@@ -1,10 +1,12 @@
 module Divert
   class Configuration
     attr_accessor :save_to_db
+    attr_accessor :redirect_from_view
     attr_accessor :controller
 
     def initialize
       @save_to_db = true
+      @redirect_from_view = false
     end
   end
 end
